48852988d0413ce903f92893f1b9f92ef54ba66c
Costanza <costanza.calzolari@gmail.com>
2018-11-28 22:32:23 +0100

    unified into one unique pipeline

--- code/run_pipeline.py
+++ code/run_pipeline.py
@@ -1,6 +1,7 @@
 from embedding.embedding import embedding
 from similarity.similarity import similarity
 from classifier.classifier import classifier
+from predict.predict import predict
 
 
 if __name__== "__main__" :
@@ -8,3 +9,4 @@ if __name__== "__main__" :
     embedding()
     similarity()
     classifier()
+    predict()
