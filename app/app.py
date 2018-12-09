696b279d54dda65cbeb00112254b3eca6c844905
brian-regan <brianmjregan@gmail.com>
2018-12-09 16:12:20 +0100

    app v1

--- app/app.py
+++ app/app.py
@@ -7,16 +7,68 @@ from utils import ui
 from utils import pipeline
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+image_style = {
+    'borderRadius':'100%',
+    'display': 'inline-block',
+    "width": "76%"}
+
+images = [html.Div(html.Img(src='assets/brian.jpg', style=image_style),
+                                style={'text-align':'center'}, className="four columns"),
+                        html.Div(html.Img(src='assets/costanza.jpeg', style=image_style),
+                                                        style={'text-align':'center'}, className="four columns"),
+                        html.Div(html.Img(src='assets/maggie.jpeg', style=image_style),
+                                 style={'text-align':'center'}, className="four columns")
+                        ]
+
+name_style = style={'textAlign':'center', 'color':'white'}
+
+names = [
+    html.Div(html.Span('Brian Regan'), className="four columns", style=name_style),
+    html.Div(html.Span('Costanza Calzolari'), className="four columns", style=name_style),
+    html.Div(html.Span('Margherita Rosnati'), className="four columns", style=name_style)
+]
 
 app.layout = html.Div([
-    dcc.Input(id='input-1-keypress', type='text', value='Sample Ticket Text',
-              style={
-                  "width": '100%'
-              }),
-    html.Div(id='output-keypress')
+
+    html.H1('Natural Language Misunderstanding', style={"textAlign":"center", 'paddingTop':"30px"}),
+    html.H2('An Interesting Subtitle', style={"textAlign":"center"}),
+    html.Div([
+        dcc.Input(id='text-input', type='text', value='Sample Ticket Text', style={'width':'100%', 'height':'200px'})
+        ], style={
+        "width": '80%',
+        "margin": "auto",
+        "marginTop": '10px',
+        "marginBottom": '50px'
+    }
+    ),
+    html.Div(id='faq-outputs',
+             style={
+                 "width": "80%",
+                 "height": "1000px",
+                 "overflow": "scroll",
+                 "margin": "auto",
+                 "marginTop": '10px',
+                 "marginBottom": '10px'
+             }),
+    html.Div(id='team-info',
+             style = {
+                 "width":"100%",
+                 "height": "400px",
+                 "background": "#333"
+             },
+             children= [
+                 html.Div(images, style={'width':'80%', 'margin':'auto', "paddingTop":"30px"}, className="row"),
+                 html.Div(names, style={'width':'80%', 'margin':'auto', "paddingTop":""}, className="row")
+
+             ]
+             ),
+    html.Div(id='footer',
+             style={
+                 "width": "100%",
+                 "height": "100px"
+             })
 ])
 
 # Inital Loading Steps (run before app)
@@ -25,8 +77,8 @@ embedder = pipeline.load_embedder(model=MODEL)
 classifier = pipeline.load_classifier(model=MODEL)
 
 
-@app.callback(Output('output-keypress', 'children'),
-              [Input('input-1-keypress', 'value')])
+@app.callback(Output('faq-outputs', 'children'),
+              [Input('text-input', 'value')])
 def update_output(input1):
 
     probs, labels = pipeline.predict(text=input1, embedder=embedder, classifier=classifier, model=MODEL)
--- app/utils/ui.py
+++ app/utils/ui.py
@@ -1,10 +1,11 @@
 import dash_html_components as html
 import numpy as np
+import pickle
 
 def create_faq_boxes(faqs):
 
     # Generates an div containing FAQ boxes
-    # faqs: a array of dictionaries with keys 'title', 'text' and 'prob', one per FAQ
+    # faqs: a array of dictionaries with keys 'title', 'text' and 'prob' and 'index', one per FAQ
 
     children = []
 
@@ -16,14 +17,26 @@ def create_faq_boxes(faqs):
 
 def create_faq_box(faq):
 
+    probability_style ={
+        "background": '#57c5f7',
+        "color": "white",
+        "padding": "5px",
+        "borderRadius": "5px"}
+
     # Generates an FAQ box
-    # dictionary with keys 'title' and 'text' and 'prob'
+    # dictionary with keys 'title' and 'text' and 'prob' and 'index'
 
     box = html.Div([
         html.H3(faq['title']),
-        html.P(faq['prob']),
-        html.P(faq['text'])
-    ])
+        html.Div([html.Span('Probability: {}'.format(faq['prob']), style=probability_style)]),
+        html.Div([html.Span(faq['text'])])
+    ],
+    style={
+        "boxShadow": "1px 2px 9px 0px",
+        "padding": "10px",
+        "margin": "5px 5px 20px 5px",
+        "borderRadius": "10px"
+    })
 
     return box
 
@@ -39,27 +52,49 @@ def faq_data_from_predictions(probs, labels):
     # These are the actual FAQ ids -1 etc. different from numpy index
     faq_indexs = labels[top_three_index]
 
-    top_three_probs = probs[top_three_index]
-    top_three_text = get_faq_texts(faq_indexs)
-    top_three_titles = get_faq_titles(faq_indexs)
+    faq_raw_answer, faq_title = load_raw_faq_data()
 
-    for prob, text, title in zip(top_three_probs, top_three_text, top_three_titles):
+    top_three_probs = probs[top_three_index]
+    top_three_text = get_faq_texts(faq_indexs, faq_raw_answer)
+    top_three_titles = get_faq_titles(faq_indexs, faq_title)
+
+
+    for prob, text, title, index in zip(top_three_probs, top_three_text, top_three_titles, faq_indexs):
         faqs.append({
-            'title':title,
-            'text':text,
-            'prob':prob
+            'title': title,
+            'text': text,
+            'prob': prob,
+            'index': index
         })
 
     print(top_three_index)
     return faqs
 
-def get_faq_texts(indexs):
-    # TODO: have this use the FAQ dataset to return the text of the FAQs
-    return np.repeat('test', len(indexs))
+def load_raw_faq_data():
+    # Load the raw title and content of FAQ for display
+    with open("../code/embedding/models/doc_data/all_docs_sep.pkl", "rb") as fp:
+        all_docs_sep = pickle.load(fp)
+    return all_docs_sep['faq_raw_answer'], all_docs_sep['faq_title']
 
-def get_faq_titles(indexs):
-    # TODO: have this use the FAQ dataset to return the title of the FAQs
-    return indexs
+def get_faq_texts(faq_indexs, faq_raw_answer):
+    texts = []
+    for index in faq_indexs:
+        if index == -1:
+            texts.append('Please answer this manually')
+        else:
+            texts.append(faq_raw_answer[index])
+
+    return texts
+
+def get_faq_titles(faq_indexs, faq_title):
+    titles = []
+    for index in faq_indexs:
+        if index == -1:
+            titles.append('No FAQ in Dataset')
+        else:
+            titles.append(faq_title[index])
+
+    return titles
 
 def rev_sort(arr, n):
     # Return args of greatest n values in arr
