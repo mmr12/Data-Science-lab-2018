1cf616ed027a8f098f8a1a71b88cb1ecddab5e13
brian-regan <brianmjregan@gmail.com>
2018-12-10 18:25:41 +0100

    added buttons to load pre saved text and added maggie picture

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 from utils import ui
 from utils import pipeline
+from utils import example_text
 
 
 app = dash.Dash(__name__)
@@ -14,11 +15,13 @@ image_style = {
     'display': 'inline-block',
     "width": "76%"}
 
+example_texts = example_text.load_example_texts()
+
 images = [html.Div(html.Img(src='assets/brian.jpg', style=image_style),
                                 style={'text-align':'center'}, className="four columns"),
                         html.Div(html.Img(src='assets/costanza.jpeg', style=image_style),
                                                         style={'text-align':'center'}, className="four columns"),
-                        html.Div(html.Img(src='assets/maggie.jpeg', style=image_style),
+                        html.Div(html.Img(src='assets/maggie_new.jpeg', style=image_style),
                                  style={'text-align':'center'}, className="four columns")
                         ]
 
@@ -35,14 +38,29 @@ app.layout = html.Div([
     html.H1('Natural Language Misunderstanding', style={"textAlign":"center", 'paddingTop':"30px"}),
     html.H2('An Interesting Subtitle', style={"textAlign":"center"}),
     html.Div([
-        dcc.Input(id='text-input', type='text', value='Sample Ticket Text', style={'width':'100%', 'height':'200px'})
+        dcc.Textarea(id='text-input',
+                     placeholder='Write or paste Ticket Text here or select an example below',
+                     style={'width':'100%', 'height':'200px'})
         ], style={
         "width": '80%',
         "margin": "auto",
         "marginTop": '10px',
-        "marginBottom": '50px'
+        "marginBottom": '10px'
     }
     ),
+    html.Div([
+        html.Button('Clear', id='clear-text', n_clicks_timestamp='0')
+    ], style={'textAlign':'center', 'width':'80%', 'margin':'auto', 'marginBottom':'10px'}),
+    html.Div([
+        ui.button('Positive Sample', 'sample-load-1'),
+        ui.button('Positive Sample', 'sample-load-2'),
+        ui.button('Negative Sample', 'sample-load-3')
+    ],
+    style={
+        'width':'80%',
+        'margin':'auto'
+    },
+    className='row'),
     html.Div(id='faq-outputs',
              style={
                  "width": "80%",
@@ -85,6 +103,25 @@ def update_output(input1):
     faqs = ui.faq_data_from_predictions(probs, labels)
 
     return ui.create_faq_boxes(faqs)
+
+@app.callback(Output('text-input', 'value'),
+              [Input('sample-load-1', 'n_clicks_timestamp'),
+               Input('sample-load-2', 'n_clicks_timestamp'),
+               Input('sample-load-3', 'n_clicks_timestamp'),
+               Input('clear-text', 'n_clicks_timestamp')])
+def on_click(btn1, btn2, btn3, clear_btn):
+    if int(btn1) > int(btn2) and int(btn1) > int(btn3) and int(btn1) > int(clear_btn):
+        msg = example_texts[0]
+    elif int(btn2) > int(btn1) and int(btn2) > int(btn3) and int(btn2) > int(clear_btn):
+        msg = example_texts[1]
+    elif int(btn3) > int(btn1) and int(btn3) > int(btn2) and int(btn3) > int(clear_btn):
+        msg = example_texts[2]
+    elif int(clear_btn) > int(btn1) and int(clear_btn) > int(btn2) and int(clear_btn) > int(btn3):
+        msg = ''
+    else:
+        msg = ''
+    return msg
+
 
 
 if __name__ == '__main__':
